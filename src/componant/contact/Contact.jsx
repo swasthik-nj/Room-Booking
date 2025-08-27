34f6d31b852414ca952e4e4c5f31bee50ef239ff
swasthik <swastiknj76@gmail.com>
2025-08-27 18:10:15 +0530

    updated responsive for this project

--- src/componant/contact/Contact.jsx
+++ src/componant/contact/Contact.jsx
@@ -1,105 +1,88 @@
 import React from "react";
 import "./contact.css";
 import { Link } from "react-router-dom";
-import InstagramIcon from '@mui/icons-material/Instagram';
-import XIcon from '@mui/icons-material/X';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import Footer from "../../footer/Footer";
 
 export default function Contact() {
   return (
-    <div style={{ height: "100%", backgroundColor: "#cfb9fb" }}>
+    <div style={{ backgroundColor: "#cfb9fb" }}>
       <div
         style={{
-          height: "100vh",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
           flexDirection: "column",
+          padding: "40px 20px",
         }}
       >
-        <h1 style={{paddingBottom:"50px",fontSize:"50px"}}>Contact Us</h1>
+        <h1 style={{ paddingBottom: "40px", fontSize: "2.5rem" }}>Contact Us</h1>
+
         <div className="contact-sep">
+          {/* Swasthik Card */}
           <div className="contact-container">
-            <div
-              style={{
-                justifyContent: "center",
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <div className="contact-img1">
-                <img src="" alt="" srcset="" />
-              </div>
+            <div className="profile">
+              <div className="contact-img1"></div>
               <h2>Swasthik NJ</h2>
             </div>
-            <div>
-              <table cellSpacing={20} style={{paddingTop:"20px"}}>
-                <tbody>
-                  <tr>
-                    <th>Email :</th>
-                    <td>swastiknj76@gmail.com</td>
-                  </tr>
-                  <tr>
-                    <th>Ph NO :</th>
-                    <td>8951192848</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <span style={{ display: "flex", alignItems: "center", gap:"20px", paddingTop:"10px" }}>
-             <Link style={{color:"black"}} to={"https://www.instagram.com/swasthik_birwa_official/"}><InstagramIcon className='C-IC' style={{ fontSize: "25px"}} /></Link> 
-              <Link style={{color:"black"}} to={"https://x.com/swasthik_nj"}><XIcon className='C-IC' style={{ fontSize: "25px"}}></XIcon></Link>
-              <Link style={{color:"black"}} to={"https://www.facebook.com/profile.php?id=100070363380180"}><FacebookIcon className='C-IC' style={{ fontSize: "25px"}}></FacebookIcon></Link>
-              <Link style={{color:"black"}} to={"https://www.linkedin.com/in/swasthik-nj/"}><LinkedInIcon className='C-IC' style={{ fontSize: "25px"}}></LinkedInIcon></Link>
-              <Link style={{color:"black"}} to={"https://github.com/swasthik-nj"}><GitHubIcon  className='C-IC' style={{ fontSize: "25px"}}></GitHubIcon></Link>
-          </span>
+
+            <table cellSpacing={15} style={{ margin: "20px auto" }}>
+              <tbody>
+                <tr>
+                  <th>Email :</th>
+                  <td>swastiknj76@gmail.com</td>
+                </tr>
+                <tr>
+                  <th>Ph No :</th>
+                  <td>8951192848</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <span className="icons">
+              <Link to="https://www.instagram.com/swasthik_birwa_official/" target="_blank"><InstagramIcon className="C-IC" /></Link>
+              <Link to="https://x.com/swasthik_nj" target="_blank"><XIcon className="C-IC" /></Link>
+              <Link to="https://www.facebook.com/profile.php?id=100070363380180" target="_blank"><FacebookIcon className="C-IC" /></Link>
+              <Link to="https://www.linkedin.com/in/swasthik-nj/" target="_blank"><LinkedInIcon className="C-IC" /></Link>
+              <Link to="https://github.com/swasthik-nj" target="_blank"><GitHubIcon className="C-IC" /></Link>
+            </span>
           </div>
 
+          {/* Sudesh Card */}
           <div className="contact-container">
-            <div
-              style={{
-                justifyContent: "center",
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-            >
-              <div className="contact-img2">
-                
-              </div>
+            <div className="profile">
+              <div className="contact-img2"></div>
               <h2>Sudesh Gowda</h2>
             </div>
-            <div>
-              <table cellSpacing={20} style={{paddingTop:"20px"}}>
-                <tbody>
-                  <tr>
-                    <th>Email :</th>
-                    <td>ssudeshgowda@gmail.com</td>
-                  </tr>
-                  <tr>
-                    <th>Ph NO :</th>
-                    <td>9632303798</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <span style={{ display: "flex", alignItems: "center", gap:"20px", paddingTop:"10px" }}>
-             <Link style={{color:"black"}}><InstagramIcon className='C-IC' style={{ fontSize: "25px"}} /></Link> 
-              <Link style={{color:"black"}}><XIcon className='C-IC' style={{ fontSize: "25px"}}></XIcon></Link>
-              <Link style={{color:"black"}}><FacebookIcon className='C-IC' style={{ fontSize: "25px"}}></FacebookIcon></Link>
-              <Link style={{color:"black"}}><LinkedInIcon className='C-IC' style={{ fontSize: "25px"}}></LinkedInIcon></Link>
-              <Link style={{color:"black"}}><GitHubIcon  className='C-IC' style={{ fontSize: "25px"}}></GitHubIcon></Link>
 
-          </span>
+            <table cellSpacing={20}  style={{ margin: "20px auto" }}>
+              <tbody>
+                <tr>
+                  <th>Email :</th>
+                  <td>ssudeshgowda@gmail.com</td>
+                </tr>
+                <tr>
+                  <th>Ph No :</th>
+                  <td>9632303798</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <span className="icons">
+              <Link><InstagramIcon className="C-IC" /></Link>
+              <Link><XIcon className="C-IC" /></Link>
+              <Link><FacebookIcon className="C-IC" /></Link>
+              <Link><LinkedInIcon className="C-IC" /></Link>
+              <Link><GitHubIcon className="C-IC" /></Link>
+            </span>
           </div>
         </div>
-        
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,72 +1,104 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "../footer/footer.css"
-import InstagramIcon from '@mui/icons-material/Instagram';
-import XIcon from '@mui/icons-material/X';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../footer/footer.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Footer() {
-  
   return (
-    <div className='foot-container'>
+    <div className="foot-container">
       <div className="footer-sep">
         <div className="gratitude">
-          <h1 className='footer-heading'>Swadhisht</h1>
+          <h1 className="footer-heading">Swadhisht</h1>
           <p>Thank you so much for your valuable time</p>
-          <div className='platform-icons'>
-          <span style={{ display: "flex", alignItems: "center", gap:"20px", paddingTop:"30px" }}>
-             <Link to='https://github.com/settings/two_factor_checkup?'><InstagramIcon className='IC' style={{ fontSize: "30px"}} /></Link> 
-              <XIcon className='IC' style={{ fontSize: "30px"}}></XIcon>
-              <FacebookIcon className='IC' style={{ fontSize: "30px"}}></FacebookIcon>
-              <LinkedInIcon className='IC' style={{ fontSize: "30px"}}></LinkedInIcon>
-          </span>
+          <div className="platform-icons">
+            <span
+              style={{
+                display: "flex",
+                alignItems: "center",
+                gap: "20px",
+                paddingTop: "30px",
+              }}
+            >
+              <Link to="https://www.instagram.com/swasthik_birwa_official/">
+                <InstagramIcon className="IC" style={{ fontSize: "30px" }}/>
+              </Link>
+              <Link to="https://x.com/swasthik_nj">
+                <XIcon className="IC" style={{ fontSize: "30px" }}></XIcon>
+              </Link>
+              <Link to="https://www.facebook.com/profile.php?id=100070363380180">
+                {" "}
+                <FacebookIcon
+                  className="IC"
+                  style={{ fontSize: "30px" }}
+                ></FacebookIcon>
+              </Link>
+              <Link to="https://www.linkedin.com/in/swasthik-nj">
+                {" "}
+                <LinkedInIcon
+                  className="IC"
+                  style={{ fontSize: "30px" }}
+                ></LinkedInIcon>
+              </Link>
+            </span>
           </div>
         </div>
-        <div className="footer-con-about">  
+        <div className="footer-con-about">
           <div className="footer-contact">
-            <h1 className='footer-heading'>Contact us</h1>
+            <h1 className="footer-heading">Contact us</h1>
             <div className="con-details">
-              <p>8951192848</p>
               <p>8951192848</p>
               <p>swastiknj76@gmail.com</p>
               <p>Mangalore,Karnataka</p>
             </div>
-            
           </div>
           <div className="footer-nav">
-          <h1 className='footer-heading'>Explore</h1>
+            <h1 className="footer-heading">Explore</h1>
             <ul>
               <li>
-                <Link className='footer-nav-button' to={'/'}>
-                <p>Home</p>
-                <span class="material-symbols-outlined" style={{fontSize:"20px"}}>
-                open_in_new
+                <Link className="footer-nav-button" to={"/"}>
+                  <p>Home</p>
+                  <span
+                    class="material-symbols-outlined"
+                    style={{ fontSize: "20px" }}
+                  >
+                    open_in_new
                   </span>
                 </Link>
               </li>
               <li>
-                <Link className='footer-nav-button' to={'/about'}>
-                <p>About us</p>
-                <span class="material-symbols-outlined" style={{fontSize:"20px"}}>
-                open_in_new
+                <Link className="footer-nav-button" to={"/about"}>
+                  <p>About us</p>
+                  <span
+                    class="material-symbols-outlined"
+                    style={{ fontSize: "20px" }}
+                  >
+                    open_in_new
                   </span>
                 </Link>
               </li>
               <li>
-                <Link className='footer-nav-button' to={''}>
+                <Link className="footer-nav-button" to={""}>
                   <p>Services</p>
-                    <span class="material-symbols-outlined" style={{fontSize:"20px"}}>
-                     open_in_new
-                    </span>
+                  <span
+                    class="material-symbols-outlined"
+                    style={{ fontSize: "20px" }}
+                  >
+                    open_in_new
+                  </span>
                 </Link>
               </li>
               <li>
-                <Link className='footer-nav-button' to={'/contact'}>
+                <Link className="footer-nav-button" to={"/contact"}>
                   <p>Contact us</p>
-                    <span class="material-symbols-outlined" style={{fontSize:"20px"}}>
-                      open_in_new
-                    </span>
+                  <span
+                    class="material-symbols-outlined"
+                    style={{ fontSize: "20px" }}
+                  >
+                    open_in_new
+                  </span>
                 </Link>
               </li>
             </ul>
@@ -74,12 +106,15 @@ export default function Footer() {
         </div>
       </div>
       <div class="copyright">
-        <p>Copyright &copy; 2025. All Rights Reserved. — Designed with love by <a href="SSS">Swa NJ</a></p>
+        <p>
+          Copyright &copy; 2025. All Rights Reserved. — Designed with love by{" "}
+          <a href="SSS">Swa NJ</a>
+        </p>
         <ul>
           <li>Term & Condition</li>
           <li>Privacy Policy</li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
